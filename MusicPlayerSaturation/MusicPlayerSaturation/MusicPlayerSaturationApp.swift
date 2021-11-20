2f13adb7d9bb0c1d85bd0ef30019c95991a169c4
KARAN BHATIA <82643039+Karanbhatiakb@users.noreply.github.com>
2021-11-20 22:05:40 +0530

    Add files via upload

---
+++ MusicPlayerSaturation/MusicPlayerSaturation/MusicPlayerSaturationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerSaturationApp.swift
+//  MusicPlayerSaturation
+//
+//  Created by Karan Bhatia on 20/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerSaturationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
